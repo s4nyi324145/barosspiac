6bb956058f8e23f625d3f5b3c0831cc5f56176fc
s4nyi324145 <sandorszucsm6@gmail.com>
2026-04-26 23:13:23 +0200

    admin panel fix

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://nodejs214.dszcbaross.edu.hu/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
